2103cc9bd5180e9f6a460bca579ed4c105bcf915
Joel Midstjärna <joel.midstjarna@gmail.com>
2019-10-03 22:41:52 +0200

    Styling fixes

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/GeertJohan/go.rice"
 )
@@ -11,34 +11,15 @@ import (
 var box *rice.Box
 
 func main() {
-	fmt.Println("Starting BurnRate server")
-	box = rice.MustFindBox("templates")
-	http.HandleFunc("/", serverHandler)
-	err := http.ListenAndServe(":8080", nil)
+	var port string
+	flag.StringVar(&port, "p", "8080", "Network port to listen to")
+	flag.Parse()
+
+	fmt.Println("Starting BurnRate server on port " + port)
+
+	http.Handle("/", http.FileServer(rice.MustFindBox("templates").HTTPBox()))
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Printf("Error! %s\n", err)
-	}
-}
-
-func serverHandler(w http.ResponseWriter, r *http.Request) {
-	fileName := r.URL.Path
-	if len(r.URL.Path) > 0 {
-		fileName = r.URL.Path[1:] // Remove '/'
-	}
-	if fileName == "" {
-		// Default is index page
-		fileName = "index.html"
-	}
-	bytes, err := box.Bytes(fileName)
-	if err != nil || len(bytes) == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "Unable to find: %s!", fileName)
-	} else {
-		if filepath.Ext(fileName) == ".html" {
-			w.Header().Set("Content-Type", "text/html")
-		} else {
-			w.Header().Set("Content-Type", "image/png")
-		}
-		w.Write(bytes)
 	}
 }
